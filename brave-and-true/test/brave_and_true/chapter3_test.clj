fd97eda49fe63e5261f5ca382b4d628b735bef86
makutak <takkouno@gmail.com>
2018-07-31 03:51:21 +0900

    フォームのテスト

--- brave-and-true/test/brave_and_true/chapter3_test.clj
+++ brave-and-true/test/brave_and_true/chapter3_test.clj
@@ -3,6 +3,8 @@
             [brave-and-true.core :refer :all]
             [brave-and-true.chapter3 :refer :all]))
 
-(deftest foo-test
-  (testing "foo-test"
-    (is (= "this is chapter-3" (foo)))))
+(deftest form
+  (testing "form"
+    (is (= 6 (+ 1 2 3)))
+    (is (= (str "It was the panda " "in the library " "with a dust buster")
+           "It was the panda in the library with a dust buster"))))
