9ec9e2640310693f3b7d664175dd3d2086f45af6
makutak <takkouno@gmail.com>
2018-08-01 03:28:37 +0900

    ifの挙動

--- brave-and-true/test/brave_and_true/chapter3_test.clj
+++ brave-and-true/test/brave_and_true/chapter3_test.clj
@@ -8,3 +8,19 @@
     (is (= 6 (+ 1 2 3)))
     (is (= (str "It was the panda " "in the library " "with a dust buster")
            "It was the panda in the library with a dust buster"))))
+
+
+(deftest control-flow
+  (testing "if"
+    (is (= "By Zeus's hammer!"
+           (if true
+             "By Zeus's hammer!"
+             "By Aquaman's trident!")))
+    (is (= "By Aquaman's trident!"
+           (if false
+             "By Zeus's hammer!"
+             "By Aquaman's trident!")))
+    ;;falseに対応するのがないときはnilが返る。
+    (is (= nil
+           (if false
+             "By Zeu's hammer!")))))
