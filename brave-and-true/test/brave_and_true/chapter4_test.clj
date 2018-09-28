22e01555b9a0e32932652abb2d75c1b2dbf0e794
makutak <takkouno@gmail.com>
2018-09-28 21:59:38 +0900

    mapのkeywordは関数として使えるので、mapを使い、mapのcollectionからvalueを取り出しリスト化することもできる。

--- brave-and-true/test/brave_and_true/chapter4_test.clj
+++ brave-and-true/test/brave_and_true/chapter4_test.clj
@@ -60,3 +60,14 @@
            (stats [3 4 10])))
     (is (= '(144 5 144/5)
            (stats [80 1 44 13  6])))))
+
+(def identities
+  [{:alias "Batman" :real "Bruce Wayne"}
+   {:alias "Spider-Man" :real "Peter Parker"}
+   {:alias "Santa" :real "Your mom"}
+   {:alias "Easter Bunny" :real "Your dad"}])
+
+(deftest using-map-test
+  (testing "map get value from a collection of map data structures"
+    (is (= '("Bruce Wayne" "Peter Parker" "Your mom" "Your dad")
+           (map :real identities)))))
