0045242ffbb93924bd18ed84bfebde7b08dcc537
makutak <takkouno@gmail.com>
2018-10-01 09:31:17 +0900

    take-whileは述語が成り立つまでのデータを返す?

--- brave-and-true/test/brave_and_true/chapter4_test.clj
+++ brave-and-true/test/brave_and_true/chapter4_test.clj
@@ -115,3 +115,23 @@
   (testing "drop returns the sequence with the first n element removed"
     (is (= '(4 5 6 7 8 9 10)
            (drop 3 (range 1 11))))))
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(def less-than-3-month '({:month 1, :day 1, :human 5.3, :critter 2.3}
+                         {:month 1, :day 2, :human 5.1, :critter 2.0}
+                         {:month 2, :day 1, :human 4.9, :critter 2.1}
+                         {:month 2, :day 2, :human 5.0, :critter 2.5}))
+
+(deftest take-while-test
+  (testing "return month less than 3 data"
+    (is (= less-than-3-month
+           (take-while #(< (:month %) 3) food-journal)))))
