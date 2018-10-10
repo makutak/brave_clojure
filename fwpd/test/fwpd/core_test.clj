df97009a662df5d956cd895c5d8f21568225f8ba
makutak <takkouno@gmail.com>
2018-10-10 09:17:18 +0900

    vamp keyの変換関数

--- fwpd/test/fwpd/core_test.clj
+++ fwpd/test/fwpd/core_test.clj
@@ -6,3 +6,11 @@
   (testing "slurp reading file and return string"
     (is (= "Edward Cullen,10\nBella Swan,0\nCharlie Swan,0\nJacob Black,3\nCarlisle Cullen,6\n"
            (slurp filename)))))
+
+(deftest convert-test
+  (testing "convert resturns passed value if vamp-key is ':name'."
+    (is (= "hoge"
+           (convert :name "hoge"))))
+  (testing "convert returns int value if vamp-key is :glitter-index"
+    (is (= 3
+           (convert :glitter-index "3")))))
