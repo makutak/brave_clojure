c1a8e0795dae71a48a20b323982f728c51d67c1d
makutak <takkouno@gmail.com>
2018-10-17 09:14:30 +0900

    再帰を使ってリストの合計値を求める

--- brave-and-true/src/brave_and_true/chapter5.clj
+++ brave-and-true/src/brave_and_true/chapter5.clj
@@ -1,1 +1,10 @@
 (ns brave-and-true.chapter5 (:gen-class))
+
+(defn sum
+  ([vals]
+   (sum vals 0))
+  ([vals accumulating-total]
+   (if (empty? vals)
+     accumulating-total
+     (sum (rest vals)
+          (+ (first vals) accumulating-total)))))
--- brave-and-true/test/brave_and_true/chapter5_test.clj
+++ brave-and-true/test/brave_and_true/chapter5_test.clj
@@ -2,3 +2,16 @@
   (:require [clojure.test :refer :all]
             [brave-and-true.core :refer :all]
             [brave-and-true.chapter5 :refer :all]))
+
+
+(deftest sum-test
+  (testing "sum from list"
+    (is (= 10
+           (sum '(10))))
+    (is (= 55
+           (sum '(1 2 3 4 5 6 7 8 9 10)))))
+  (testing "sum from list and initial value"
+    (is (= 15
+           (sum '(10) 5)))
+    (is (= 155
+           (sum '(1 2 3 4 5 6 7 8 9 10) 100)))))
