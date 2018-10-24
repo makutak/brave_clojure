365fe14a09ead36d17ca6d04d5e5713f89203cd1
makutak <takkouno@gmail.com>
2018-10-25 08:41:02 +0900

    テスト追加

--- pegthing/test/pegthing/core_test.clj
+++ pegthing/test/pegthing/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [pegthing.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest tri*-test
+  (testing "return triaugnler number sequence."
+    (is (= '(1 3 6 10 15)
+           (take 5 (tri*))))
+    (is (= '(1 3 6 10 15 21 28)
+           (take 7 (tri*))))
+    (is (= '(0 1 3 6 10)
+           (take 5 (tri* 0 0))))))
