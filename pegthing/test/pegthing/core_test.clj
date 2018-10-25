2ce85b7ffc167722affdb88fb022c54c75ccf241
makutak <takkouno@gmail.com>
2018-10-25 09:02:11 +0900

    引数が三角数かどうかのテストを追加

--- pegthing/test/pegthing/core_test.clj
+++ pegthing/test/pegthing/core_test.clj
@@ -10,3 +10,12 @@
            (take 7 (tri*))))
     (is (= '(0 1 3 6 10)
            (take 5 (tri* 0 0))))))
+
+(deftest triangular?-test
+  (testing "return args is the number triangular number"
+    (is (= true
+           (triangular? 6)))
+    (is (= false
+           (triangular? 4)))
+    (is (= true
+           (triangular? 5050)))))
