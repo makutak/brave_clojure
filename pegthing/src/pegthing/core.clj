2b80b3485c606319091f04d3f9c57ed3e8008498
makutak <takkouno@gmail.com>
2018-10-31 08:55:27 +0900

    与えられた行数のboardを返す

--- pegthing/src/pegthing/core.clj
+++ pegthing/src/pegthing/core.clj
@@ -72,6 +72,17 @@
             pegged-board
             [connect-right connect-down-left connect-down-right])))
 
+(defn new-board
+  "Creates a new board with the given number of rows"
+  [rows]
+  (let [initial-board {:rows rows}
+        max-pos (row-tri rows)]
+    (reduce (fn [board pos]
+              (add-pos board max-pos pos))
+            initial-board
+            (range 1 (inc max-pos)))))
+
+
 ;; (defn -main
 ;;   "I don't do a whole lot ... yet."
 ;;   [& args]
