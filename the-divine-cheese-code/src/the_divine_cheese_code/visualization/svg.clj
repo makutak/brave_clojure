1864ad7073380f191369c5e722793fa10c8ae906
makutak <takkouno@gmail.com>
2018-11-21 21:29:43 +0900

    lat lngのmapのvectorを空白区切りにの文字列に変換する

--- the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
+++ the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -4,3 +4,7 @@
   "Converting lat/lng map to commpa-separated string"
   [latlng]
   (str (:lat latlng) ", " (:lng latlng)))
+
+(defn points
+  [locations]
+  (clojure.string/join " " (map latlng->point locations)))
