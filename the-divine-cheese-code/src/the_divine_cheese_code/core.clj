50180d1b1131242e3e7adf23504eb09eed0c0513
makutak <takkouno@gmail.com>
2018-11-22 19:52:00 +0900

    別ファイルの関数を使えるように

--- the-divine-cheese-code/src/the_divine_cheese_code/core.clj
+++ the-divine-cheese-code/src/the_divine_cheese_code/core.clj
@@ -1,5 +1,9 @@
-(ns the-divine-cheese-code.core
-  (:gen-class))
+(ns the-divine-cheese-code.core)
+;; Ensure that the SvG code is evaluated
+(require 'the-divine-cheese-code.visualization.svg)
+;; Refer the namespace so that you don't have to use the
+;; fully qualified name to reference svg function
+(refer 'the-divine-cheese-code.visualization.svg)
 
 (defn -main
   "I don't do a whole lot ... yet."
