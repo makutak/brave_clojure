e412a937e81492a5c60b59a0e6c8bb43e93bc39e
makutak <takkouno@gmail.com>
2018-11-26 21:26:54 +0900

    テスト書いた

--- the-divine-cheese-code/test/the_divine_cheese_code/visualization/svg_test.clj
+++ the-divine-cheese-code/test/the_divine_cheese_code/visualization/svg_test.clj
@@ -12,3 +12,14 @@
     (is (= "hoge, fuga foo, bar"
            (points [{:lat "hoge" :lng "fuga"}
                     {:lat "foo"  :lng "bar"}])))))
+
+(def testdata [{:min 100 :max 1000}
+               {:min 10 :max 100}
+               {:min 91 :max 1024}])
+
+(deftest comparator-over-maps-test
+  (testing "return new map"
+    (is (= {:min 10 :max 100}
+           ((comparator-over-maps clojure.core/min [:min :max]) testdata)))
+    (is (= {:min 100 :max 1024}
+           ((comparator-over-maps clojure.core/max [:min :max]) testdata)))))
