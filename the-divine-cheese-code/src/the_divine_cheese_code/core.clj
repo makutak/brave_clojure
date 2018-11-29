cfba84a5e3caf03e68dfb6c99f8ce0b6e08db98c
makutak <takkouno@gmail.com>
2018-11-29 21:24:26 +0900

    lein runコマンドでsvgを開くように

--- the-divine-cheese-code/src/the_divine_cheese_code/core.clj
+++ the-divine-cheese-code/src/the_divine_cheese_code/core.clj
@@ -1,5 +1,8 @@
-(ns the-divine-cheese-code.core)
-(use '[the-divine-cheese-code.visualization.svg :as svg :only [points]])
+(ns the-divine-cheese-code.core
+  (:require [clojure.java.browse :as browse]
+            [the-divine-cheese-code.visualization.svg :refer [xml]])
+  (:gen-class))
+
 
 (def heists [{:location "Cologne, Germany"
               :cheese-name "Archbishop Hildebold's Cheese Pretzel"
@@ -22,6 +25,23 @@
               :lat 41.90
               :lng 12.45}])
 
+(defn url
+  [filename]
+  (str "file:///"
+       (System/getProperty "user.dir")
+       "/"
+       filename))
+
+(defn template
+  [contents]
+  (str "<style> polyline { fill:none; stroke:#5881d8; stroke-width:3} </style>"
+       contents))
+
 (defn -main
   [& args]
-  (println (svg/points heists)))
+  (let [filename "map.html"]
+    (->> heists
+         (xml 50 100)
+         template
+         (spit filename))
+    (browse/browse-url (url filename))))
