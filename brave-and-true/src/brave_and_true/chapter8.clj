dcdf0757dca23b8bff9188e1be9bffd789d33715
makutak <takkouno@gmail.com>
2018-12-13 22:17:51 +0900

    マクロ内で変数や関数を展開するときはquoteを使う

--- brave-and-true/src/brave_and_true/chapter8.clj
+++ brave-and-true/src/brave_and_true/chapter8.clj
@@ -1,2 +1,8 @@
 (ns brave-and-true.chapter8
   (:require [clojure.core]))
+
+(defmacro my-print
+  [expression]
+  (list 'let ['result expression]
+        (list 'println 'result)
+        'result))
--- brave-and-true/test/brave_and_true/chapter8_test.clj
+++ brave-and-true/test/brave_and_true/chapter8_test.clj
@@ -2,3 +2,13 @@
   (:require [clojure.test :refer :all]
             [brave-and-true.core :refer :all]
             [brave-and-true.chapter8 :refer :all]))
+
+(deftest my-print-test
+  (testing "my-print is resutn args"
+    (is (= "hoge"
+           (my-print "hoge"))))
+  (testing "expand let form"
+    (is (= '(let* [result "my-print!"]
+              (println result)
+              result)
+           (macroexpand '(my-print "my-print!"))))))
