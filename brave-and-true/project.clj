565445765a7256b4e2e012ebe36defd1906214c5
makutak <takkouno@gmail.com>
2018-12-30 18:29:00 +0900

    version upとplugin追加

--- brave-and-true/project.clj
+++ brave-and-true/project.clj
@@ -3,8 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/tools.trace "0.7.10"]]
   :main ^:skip-aot brave-and-true.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :test-selectors {:default (complement :not-lein)})
